ded2474f318d8ebca51b8b4282b60a9d91445a7f
Karthikeyan.E <150637194+Karthikeyaneswaran@users.noreply.github.com>
2025-09-23 20:38:03 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
